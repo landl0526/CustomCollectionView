9a7df858a96e14dd86a82807c5b3da743085d192
Land Lu <v-luting@microsoft.com>
2018-05-23 12:20:01 +0800

    Add project files.

---
+++ CustomFlowlayoutDemo/MyCollectionViewCell.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+using Foundation;
+using UIKit;
+
+namespace CustomFlowlayoutDemo
+{
+    public partial class MyCollectionViewCell : UICollectionViewCell
+    {
+        public static readonly NSString Key = new NSString("MyCollectionViewCell");
+        public static readonly UINib Nib;
+
+        static MyCollectionViewCell()
+        {
+            Nib = UINib.FromName("MyCollectionViewCell", NSBundle.MainBundle);
+        }
+
+        protected MyCollectionViewCell(IntPtr handle) : base(handle)
+        {
+            // Note: this .ctor should not contain any initialization logic.
+
+            ContentView.BackgroundColor = UIColor.Red;
+        }
+
+        private string myStr;
+        public string MyStr
+        {
+            set
+            {
+                myStr = value;
+                MyLabel.Text = myStr;
+            }
+            get
+            {
+                return myStr;
+            }
+        }
+    }
+}
---
+++ CustomFlowlayoutDemo/ViewController.cs
@@ -0,0 +1,52 @@
+﻿using CoreGraphics;
+using Foundation;
+using System;
+using UIKit;
+
+namespace CustomFlowlayoutDemo
+{
+    public partial class ViewController : UIViewController
+    {
+		public ViewController (IntPtr handle) : base (handle)
+		{
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+            // Perform any additional setup after loading the view, typically from a nib.
+
+            LineLayout lineLayout = new LineLayout();
+
+            UICollectionView collectionView = new UICollectionView(View.Bounds, lineLayout);
+            View.AddSubview(collectionView);
+            collectionView.DataSource = new MyCollectionViewSource();
+            collectionView.BackgroundColor = UIColor.White;
+
+            collectionView.RegisterNibForCell(UINib.FromName("MyCollectionViewCell", null), "Cell");
+		}
+
+		public override void DidReceiveMemoryWarning ()
+		{
+			base.DidReceiveMemoryWarning ();
+			// Release any cached data, images, etc that aren't in use.
+		}
+    }
+
+    public class MyCollectionViewSource : UICollectionViewDataSource
+    {
+        public override UICollectionViewCell GetCell(UICollectionView collectionView, NSIndexPath indexPath)
+        {
+            MyCollectionViewCell cell = collectionView.DequeueReusableCell("Cell", indexPath) as MyCollectionViewCell;
+
+            cell.MyStr = "label" + indexPath.Row;
+
+            return cell;
+        }
+
+        public override nint GetItemsCount(UICollectionView collectionView, nint section)
+        {
+            return 20;
+        }       
+    }
+}
